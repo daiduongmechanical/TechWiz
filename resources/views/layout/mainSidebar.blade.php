f5e0ebfda284ec85ef6aec6ee024184f182554b3
duong <daiduong.ducthanh@gmail.com>
2023-08-01 13:43:49 +0700

    updadte more

--- resources/views/layout/mainSidebar.blade.php
+++ resources/views/layout/mainSidebar.blade.php
@@ -36,36 +36,29 @@
                with font-awesome or any other icon font library -->
                 <li class="nav-item menu-open">
                     <a href="#" class="nav-link active">
-                        <i class="nav-icon fas fa-tachometer-alt"></i>
+                        <i class=" nav-icon dfas fa-user"></i>
                         <p>
-                            Starter Pages
+                            User management
                             <i class="right fas fa-angle-left"></i>
                         </p>
                     </a>
                     <ul class="nav nav-treeview">
                         <li class="nav-item">
-                            <a href="#" class="nav-link active">
+                            <a href="#" class="nav-link bg-light text-dark">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Active Page</p>
+                                <p> Users management </p>
                             </a>
                         </li>
                         <li class="nav-item">
-                            <a href="#" class="nav-link">
+                            <a href="#" class="nav-link bg-light text-dark">
                                 <i class="far fa-circle nav-icon"></i>
-                                <p>Inactive Page</p>
+                                <p>Admin management</p>
+
                             </a>
                         </li>
                     </ul>
                 </li>
-                <li class="nav-item">
-                    <a href="#" class="nav-link">
-                        <i class="nav-icon fas fa-th"></i>
-                        <p>
-                            Simple Link
-                            <span class="right badge badge-danger">New</span>
-                        </p>
-                    </a>
-                </li>
+
             </ul>
         </nav>
         <!-- /.sidebar-menu -->
--- resources/views/test1.blade.php
+++ resources/views/test1.blade.php
@@ -2,87 +2,74 @@
 @section('title', 'View Product')
 @section('content')
 
-<!-- Content Header (Page header) -->
-<div class="content-header">
-    <div class="container-fluid">
-        <div class="row mb-2">
-            <div class="col-sm-6">
-                <h1 class="m-0">Starter Page</h1>
-            </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Starter Page</li>
-                </ol>
-            </div><!-- /.col -->
-        </div><!-- /.row -->
-    </div><!-- /.container-fluid -->
+
+<div class="card-body  p-2">
+    <div class=" d-flex align-items-center mb-4">
+        <h1>Discount List</h1>
+    </div>
+    <table id="discount__table" class="table table-striped table-bordered  border-2 border-dark">
+        <thead thead class="thead-dark ">
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Amount</th>
+                <th>Startdate</th>
+                <th>Enddate</th>
+                <th>Object</th>
+                <th>Action</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            <tr>
+
+
+                <td>discountid </td>
+                <td>discountname</td>
+                <td>discountamount</td>
+                <td>startdate</td>
+                <td>enddate</td>
+                <td>object</td>
+
+            </tr>
+
+            <tr>
+                <th>Id</th>
+                <th>Name</th>
+                <th>Amount</th>
+                <th>Startdate</th>
+                <th>Enddate</th>
+                <th>Object</th>
+                <th>Action</th>
+            </tr>
+            </thead>
+        <tbody>
+
+            <tr>
+
+
+                <td>discountid </td>
+                <td>discountname</td>
+                <td>discountamount</td>
+                <td>startdate</td>
+                <td>enddate</td>
+                <td>object</td>
+
+
+            </tr>
+
+        </tbody>
+
+    </table>
 </div>
-<!-- /.content-header -->
-
-<!-- Main content -->
-<div class="content">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-lg-6">
-                <div class="card">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-
-                        <p class="card-text">
-                            Some quick example text to build on the card title and make up the bulk of the card's
-                            content.
-                        </p>
-
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div>
-                </div>
-
-                <div class="card card-primary card-outline">
-                    <div class="card-body">
-                        <h5 class="card-title">Card title</h5>
-
-                        <p class="card-text">
-                            Some quick example text to build on the card title and make up the bulk of the card's
-                            content.
-                        </p>
-                        <a href="#" class="card-link">Card link</a>
-                        <a href="#" class="card-link">Another link</a>
-                    </div>
-                </div><!-- /.card -->
-            </div>
-            <!-- /.col-md-6 -->
-            <div class="col-lg-6">
-                <div class="card">
-                    <div class="card-header">
-                        <h5 class="m-0">Featured</h5>
-                    </div>
-                    <div class="card-body">
-                        <h6 class="card-title">Special title treatment</h6>
-
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-
-                <div class="card card-primary card-outline">
-                    <div class="card-header">
-                        <h5 class="m-0">Featured</h5>
-                    </div>
-                    <div class="card-body">
-                        <h6 class="card-title">Special title treatment</h6>
-
-                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-                        <a href="#" class="btn btn-primary">Go somewhere</a>
-                    </div>
-                </div>
-            </div>
-            <!-- /.col-md-6 -->
-        </div>
-        <!-- /.row -->
-    </div><!-- /.container-fluid -->
 </div>
-<!-- /.content -->
+</div>
+
+<script>
+    $(function() {
+        $('#discount__table').DataTable();
+    });
+</script>
+
 
 @endsection
