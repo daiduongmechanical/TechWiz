d2457ac9fe71845184be6c3097238f35b7fb5023
duong <daiduong.ducthanh@gmail.com>
2023-08-01 14:05:51 +0700

    update user table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('fullName');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->dateTime('dob');
+            $table->boolean("role");
             $table->rememberToken();
             $table->timestamps();
         });
