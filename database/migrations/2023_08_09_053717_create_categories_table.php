0dccb843840c808fbe1298a8faf033bb3a1b91fb
duong <daiduong.ducthanh@gmail.com>
2023-08-11 10:36:17 +0700

    update more

--- database/migrations/2023_08_09_053717_create_categories_table.php
+++ database/migrations/2023_08_09_053717_create_categories_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->increments('category_id');
             $table->string('name');
+            $table->timestamps();
         });
     }
 
--- database/migrations/2023_08_09_053816_create_products_table.php
+++ database/migrations/2023_08_09_053816_create_products_table.php
@@ -18,7 +18,7 @@ return new class extends Migration
             $table->string('name');
             $table->string('description');
             $table->float('price');
-            $table->string('category_id');
+            $table->unsignedInteger('category_id');
             $table->unsignedInteger('provider_id');
             $table->timestamps();
             $table->foreign('provider_id')->references('provider_id')->on('providers');
