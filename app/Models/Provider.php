8fc9ca3b29a8b6294e3aad05a73096a267b4be10
duong <daiduong.ducthanh@gmail.com>
2023-08-11 16:54:29 +0700

    fix

--- app/Models/Provider.php
+++ app/Models/Provider.php
@@ -18,10 +18,9 @@ class Provider extends Model
 
     ];
 
-<<<<<<< HEAD
+
     protected $primaryKey = 'provider_id';
-=======
->>>>>>> origin/hien1
+
     public function products(): HasMany
     {
         return $this->hasMany(product::class, 'product_id');
